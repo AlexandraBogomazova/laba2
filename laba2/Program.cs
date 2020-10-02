39d34141acb61ac065dabb8d4ce882e10b97a94d
Bogomazova Alexandra <alexandra.bogomazova888@gmail.com>
2020-10-02 19:03:31 +0300

    Добавьте файлы проекта.

---
+++ laba2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z;
+            Console.WriteLine("Введите x и y");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
+            z = Math.Pow(3, -x) - Math.Cos(x) + Math.Sin(2 * x * y);
+            Console.WriteLine($"z = {z}");
+            /*Console.WriteLine("z = " + z);
+            Console.WriteLine("z = {0}", z);*/
+            Console.WriteLine("Введите длины катетов x и y");
+            x = Convert.ToDouble(Console.ReadLine());
+            y = Convert.ToDouble(Console.ReadLine());
+            z = Math.Sqrt(x * x + y * y) + x + y;
+            Console.WriteLine($"Периметр = {z}");
+            z = y * x * 0.5;
+            Console.WriteLine($"Площадь = {z}");
+            Console.ReadKey();
+        }
+    }
+}
